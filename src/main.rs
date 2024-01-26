acd84d9fa98a186a319fcb87ddeb46576ab4932a
Luke Secomb <luke.secomb@live.com>
2024-01-26 14:48:24 +1100

    📦 smaller bundle size?

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use leptos::*;
 mod app;
 mod tailwind_form;
 
@@ -6,6 +5,7 @@ fn main() {
     console_error_panic_hook::set_once();
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
+    
     // mount_to_body(|cx| {
     //     view! { cx, <App/> }
     // });
